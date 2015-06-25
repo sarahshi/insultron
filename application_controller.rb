65a2a0ae4479e950013ea65583eefaa3167c71a8
Sarah Shi <sshi16@spenceschool.org>
2015-06-25 15:04:53 +0000

    first command

--- application_controller.rb
+++ application_controller.rb
@@ -1,12 +1,12 @@
 require 'bundler'
 Bundler.require
-require_relative 'models/questions.rb'
+require_relative 'models/shakespeare_random.rb'
 require_relative 'models/results.rb'
-
 
 class ApplicationController < Sinatra::Base
 
   get '/' do
+    @insult = shakespeare
     erb :index
   end
 
